04ddfeb5cd61488e3795b8195f91151129875241
Wesley H. Gimenes <wehagy+github@gmail.com>
2023-08-28 08:01:47 -0300

    Update entrypoint.sh
    Signed-off-by: Wesley H. Gimenes <wehagy+github@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 set -eux
 
+args=(
+    build-lxd "$file"
+
+    # Directory to output images
+    distrobuilder.output
+
+    # Without this cache directory the overlayfs don't work
+    --cache-dir distrobuilder.cache
+
+    # Need to adjust below to variables
+    --type=unified \ 
+    --options=image.architecture=x86_64 \
+    --options=image.release=38
+)
+
 # Loop all the *.yml and *yaml files
 # inside the images directory relative
 # to working directory
 for file in ./images/*.{yml,yaml}; do
         # Verify if output is not empty and continue
 	[[ -e "$file" ]] || continue
-	distrobuilder build-lxd "$file" \
-                distrobuilder.output \ # Directory to output images
-                --cache-dir distrobuilder.cache \ # Without this cache directory the overlayfs don't work
-		--type=unified \ # Need to adjust below to variables
-		--options=image.architecture=x86_64 \
-		--options=image.release=38
+	distrobuilder "${args[@]}"
 done
